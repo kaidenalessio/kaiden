c93be88df720f5b82eb3d5d962641b58727078c0
kaiden <kaidenalessio@gmail.com>
2020-09-02 22:44:46 +0700

    Add time system

--- kaiden.js
+++ kaiden.js
@@ -1,5 +1,16 @@
 const KAIDEN = {};
 
+KAIDEN.Time = {
+	time: 0,
+	lastTime: 0,
+	deltaTime: 0,
+	update(t) {
+		this.lastTime = this.time;
+		this.time = t;
+		this.deltaTime = this.time - this.lastTime;
+	}
+};
+
 KAIDEN.Room = {
 	canvas: document.createElement('canvas')
 };
@@ -8,5 +19,11 @@ KAIDEN.Game = {
 	start() {
 		KAIDEN.Room.canvas.style.backgroundColor = 'blue';
 		document.body.appendChild(KAIDEN.Room.canvas);
+		window.requestAnimationFrame(KAIDEN.Game.update);
+	},
+	update(t) {
+		console.log(KAIDEN.Time.deltaTime.toFixed(2), KAIDEN.Time.time.toFixed(2));
+		KAIDEN.Time.update(t);
+		window.requestAnimationFrame(KAIDEN.Game.update);
 	}
 };
